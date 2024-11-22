c226a8e286b13dbb6425914cb835d2dd7379578b
Suyash Mishra <138197273+Suyash9118@users.noreply.github.com>
2024-11-22 11:45:17 +0000

    config file done

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,8 @@
+const AccountId = 12345;
+let AccountEmail = "mishrasuyash58@gmail.com"
+var AccountPassword = '12344';
+//AccountId=2;
+AccountEmail = "hlo@gmail.com"
+AccountPassword=23334433;
+console.log(AccountEmail);
+console.log(AccountPassword) ;
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,17 @@
+console.log("suyash");
+let name="suyash";
+let isloggedIn = false;
+let age= 18;
+//number
+//boolean=>true/false
+//null
+//undefind
+// symbol=>unique
+
+
+
+
+// object
+
+console.log(typeof name)
+console.log(typeof null)
