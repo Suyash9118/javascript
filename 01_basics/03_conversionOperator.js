14eaa1b3f80669f884a5130d0fbd4e00f696b53f
Suyash Mishra <138197273+Suyash9118@users.noreply.github.com>
2024-11-22 12:02:42 +0000

    config file done

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,17 @@
+let score = '33'
+console.log(typeof score);
+console.log(typeof(score));
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);
+//"33"=>33
+//"33abc"=>NaN=>type(number)
+let isloggedIn= "";
+let booleanIsLoggedIn=Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+//1=>true ; 0=>false
+// ""=> false
+//"hitesh"=> true
+let someNumber =33
+let StringNumber = String(someNumber)
+console.log(someNumber)
+console.log(typeof String(someNumber));
